6af899b50da4aef5c889e9983b7aad904edd4c7d
EC2 Default User <ec2-user@ip-172-31-35-125.us-east-2.compute.internal>
2019-03-09 20:12:15 +0000

    Finished Seeding data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,9 @@
      body: RandomData.random_paragraph
    )
  end
- 
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "Seeds Assignment Title", body: "Seeds Assignment Body")
+ puts "#{Post.count}"
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
