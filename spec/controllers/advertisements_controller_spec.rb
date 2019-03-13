fbda8a611cebfb502f1954f12521dc8d84afada5
EC2 Default User <ec2-user@ip-172-31-35-125.us-east-2.compute.internal>
2019-03-13 22:57:55 +0000

    spec test

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -17,14 +17,30 @@ RSpec.describe AdvertisementsController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
+  
+  
+  it "assigns [my_ad] to @advertisements" do
+     get :index
+     expect(assigns(:advertisements)).to eq([my_ad])
+   end
+ end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, {id: my_ad.id}
       expect(response).to have_http_status(:success)
     end
+    
+    it "renders the #show view" do
+      get :show, {id: my_ad.id}
+      expect(response).to have_http_status(:success)
   end
+  
+    it "assigns my_ad to @advertisement" do
+      get :show, {id: my_ad.id}
+      expect(assigns(:advertisement)).to eq(my_ad)
+    end
+   end
 
   describe "GET #new" do
     it "returns http success" do
