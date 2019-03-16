692d38d8c5ebdc6834bddb0eeafa633876424b0d
EC2 Default User <ec2-user@ip-172-31-35-125.us-east-2.compute.internal>
2019-03-16 17:02:47 +0000

    advertisements spec

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -3,13 +3,26 @@ class AdvertisementsController < ApplicationController
     @advertisements = Advertisement.all
   end
 
-  def show
-    @advertisement = Advertisement.find(params[:id])
-  end
+   def show
+    @advertisement = Advertisement.find(params[:id]) 
+   end
 
   def new
+    @advertisement = Advertisement.new
   end
 
   def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params = params[:advertisement][:price]
+    
+    if @advertisement.save 
+       flash[:notice] = "Advertisement was saved."
+       redirect_to @advertisement
+    else
+      flash[:error] = "There was an error saving the advertisement. Please try again"
+      render :new
+    end
   end
 end
--- spec/models/advertisement_spec.rb
+++ spec/models/advertisement_spec.rb
@@ -1,21 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Advertisement, type: :model do
-  let (:advertisement) { Advertisement.create! }
-  
-describe "attributes" do
-  it "should respond to title" do
-      expect(advertisement).to respond_to(:title)
-  end
-  
- it "should respond to copy"  do
-     expect(advertisement).to respond_to(:copy)
-  end
-  
-  it "should respond to price" do
-      expect(advertisement).to respond_to(:price)
-  end
-  
-  end
-  
+  pending "add some examples to (or delete) #{__FILE__}"
 end
