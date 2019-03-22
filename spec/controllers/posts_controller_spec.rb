7c120a3a49cd195c1dd4ca1b058bb10aac2d9761
EC2 Default User <ec2-user@ip-172-31-35-125.us-east-2.compute.internal>
2019-03-22 22:22:33 +0000

    First commit

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -38,18 +38,18 @@ RSpec.describe PostsController, type: :controller do
     end
  
     describe "POST create" do
- # #4
+ 
       it "increases the number of Post by 1" do
         expect{ post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
       end
  
- # #5
+ 
       it "assigns the new post to @post" do
         post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
         expect(assigns(:post)).to eq Post.last
       end
  
- # #6
+
       it "redirects to the new post" do
         post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
         expect(response).to redirect_to Post.last
@@ -58,32 +58,68 @@ RSpec.describe PostsController, type: :controller do
     
      describe "GET show" do
      it "returns http success" do
- # #16
+ 
        get :show, params: { id: my_post.id }
        expect(response).to have_http_status(:success)
      end
      it "renders the #show view" do
- # #17
+
        get :show, params: { id: my_post.id }
        expect(response).to render_template :show
      end
  
      it "assigns my_post to @post" do
        get :show, params: { id: my_post.id }
- # #18
+ 
        expect(assigns(:post)).to eq(my_post)
      end
    end
     
+  describe "GET edit" do
+     it "returns http success" do
+       get :edit, params: { id: my_post.id }
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #edit view" do
+       get :edit, params: { id: my_post.id }
+ 
+       expect(response).to render_template :edit
+     end
+ 
+ 
+     it "assigns post to be updated to @post" do
+       get :edit, params: { id: my_post.id }
+ 
+       post_instance = assigns(:post)
+ 
+       expect(post_instance.id).to eq my_post.id
+       expect(post_instance.title).to eq my_post.title
+       expect(post_instance.body).to eq my_post.body
+     end
+   end
+    
+    describe "PUT update" do
+     it "updates post with expected attributes" do
+       new_title = RandomData.random_sentence
+       new_body = RandomData.random_paragraph
+ 
+       put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+ 
+       updated_post = assigns(:post)
+       expect(updated_post.id).to eq my_post.id
+       expect(updated_post.title).to eq new_title
+       expect(updated_post.body).to eq new_body
+     end
+ 
+     it "redirects to the updated post" do
+       new_title = RandomData.random_sentence
+       new_body = RandomData.random_paragraph
+ 
+       put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+       expect(response).to redirect_to my_post
+     end
+   end
     
     
-    
-
- #  describe "GET edit" do
- #    it "returns http success" do
- #      get :edit
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
-
 end
