f6080b860ced60ba3a87a05ebba078379f6063eb
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-04-27 16:38:57 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
 
   resources :questions
-
+  resources :sponsored_posts
   resources :advertisements
   resources :topics do
   resources :posts, except: [:index]
---
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe SponsoredPostsController, type: :controller do
+
+   describe "GET edit" do
+     it "returns http success" do
+       get :edit, params: { topic_id: my_topic.id, id: my_post.id }
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #edit view" do
+        get :edit, params: { topic_id: my_topic.id, id: my_post.id }
+ 
+       expect(response).to render_template :edit
+     end
+ 
+ 
+     it "assigns post to be updated to @post" do
+        get :edit, params: { topic_id: my_topic.id, id: my_post.id }
+ 
+       post_instance = assigns(:post)
+ 
+       expect(post_instance.id).to eq my_post.id
+       expect(post_instance.title).to eq my_post.title
+       expect(post_instance.body).to eq my_post.body
+     end
+   end
+    
+     describe "GET show" do
+         let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
+ let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+     it "returns http success" do
+ 
+       get :show, params: { topic_id: my_topic.id, id: my_post.id }
+       expect(response).to have_http_status(:success)
+     end
+     it "renders the #show view" do
+
+       get :show, params: { topic_id: my_topic.id, id: my_post.id }
+       expect(response).to render_template :show
+     end
+ 
+     it "assigns my_post to @post" do
+       get :show, params: { topic_id: my_topic.id, id: my_post.id }
+ 
+       expect(assigns(:post)).to eq(my_post)
+     end
+   end
+   
+     describe "GET edit" do
+     it "returns http success" do
+       get :edit, params: { topic_id: my_topic.id, id: my_post.id }
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #edit view" do
+        get :edit, params: { topic_id: my_topic.id, id: my_post.id }
+ 
+       expect(response).to render_template :edit
+     end
+ 
+ 
+     it "assigns post to be updated to @post" do
+        get :edit, params: { topic_id: my_topic.id, id: my_post.id }
+ 
+       post_instance = assigns(:post)
+ 
+       expect(post_instance.id).to eq my_post.id
+       expect(post_instance.title).to eq my_post.title
+       expect(post_instance.body).to eq my_post.body
+     end
+   end
+    
+
+
+
+
+end
