7122e2bdb3137b155a3489cc64194475e8c9e9e7
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-04-28 20:00:48 +0000

    post_controller

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -48,29 +48,25 @@ RSpec.describe SponsoredPostsController, type: :controller do
      end
    end
    
-     describe "GET edit" do
-     it "returns http success" do
-       get :edit, params: { topic_id: my_topic.id, id: my_post.id }
-       expect(response).to have_http_status(:success)
-     end
+    describe "GET new" do
+      it "returns http success" do
+       get :new, params: { topic_id: my_topic.id }
+        expect(response).to have_http_status(:success)
+      end
  
-     it "renders the #edit view" do
-        get :edit, params: { topic_id: my_topic.id, id: my_post.id }
+ # #2
+      it "renders the #new view" do
+        get :new, params: { topic_id: my_topic.id }
+        expect(response).to render_template :new
+      end
  
-       expect(response).to render_template :edit
-     end
+ # #3
+      it "instantiates @post" do
+        get :new, params: { topic_id: my_topic.id }
+        expect(assigns(:post)).not_to be_nil
+      end
+    end
  
- 
-     it "assigns post to be updated to @post" do
-        get :edit, params: { topic_id: my_topic.id, id: my_post.id }
- 
-       post_instance = assigns(:post)
- 
-       expect(post_instance.id).to eq my_post.id
-       expect(post_instance.title).to eq my_post.title
-       expect(post_instance.body).to eq my_post.body
-     end
-   end
     
 
 
