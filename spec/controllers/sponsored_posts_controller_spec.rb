85ac3218f5cc15c23d7b3a70e74a09e412c09508
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-04-28 20:53:35 +0000

    sponsored_posts_controler

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPostsController, type: :controller do
+    
+    let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
+ 
+    let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
    describe "GET edit" do
      it "returns http success" do
