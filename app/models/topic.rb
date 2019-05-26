fb3b48dcbab86cb27d5c87d7ef8bf85103e5dd6a
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-05-26 01:38:06 +0000

    Finished validations_assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :sponsored_posts, dependent: :destroy
-    validates :name, lenght: { minimum: 5 } presence: true
-    validates :description, length: {minimum: 15 } presence: true
+    validates :name, length: { minimum: 5 }, presence: true
+    validates :description, length: {minimum: 15 }, presence: true
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,31 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-    let(:name) { RandomData.random_sentence }
-   let(:description) { RandomData.random_paragraph }
-   let(:public) { true }
-   let(:topic) { Topic.create!(name: name, description: description) }
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+  let(:public) { true }
+  let(:topic) { Topic.create!(name: name, description: description) }
    ## it { is_expected.to have_many(:posts) }
-   it "has many posts" do 
-   expect(topic).to have_many(:posts)
-   
+  it "has many posts" do 
+  expect(topic).to have_many(:posts)
   
-   end
-   
-    it { should have_many(:posts) }
-   it { should validate_lenght_of(:name).is_at_least(5) }
+   it { should have_many(:posts) }
+   it { should validate_length_of(:name).is_at_least(5) }
    it { should validate_length_of(:description).is_at_least(15) }
    it {should validate_presence_of(:description) }
    it {should validate_presence_of(:name) }
    
-   describe "attributes" do
+  
+end
+   
+ 
+   
+describe "attributes" do
      it "has name, description, and public attributes" do
        expect(topic).to have_attributes(name: name, description: description, public: public)
-     end
+end
 
  
-     it "is public by default" do
+   it "is public by default" do
        expect(topic.public).to be(true)
-     end
    end
-end
+  end
+ end
