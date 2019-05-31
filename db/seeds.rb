67095d23b1c75ee59bbe732c7be67ffce037e437
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-05-31 23:42:45 +0000

    posts show agreement

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,20 @@
 
  require 'random_data'
 
+ 
+
  Post.destroy_all
  Comment.destroy_all
+ 
+  5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+ 
  
  15.times do
    Topic.create!(
@@ -22,6 +34,7 @@
  50.times do
  
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -56,12 +69,19 @@
      resolved: false
       )
     end
+    
+    user = User.first
+    user.update_attributes!(
+      email: 'clifton.ferguson7@gmail.com',
+      password: 'helloworld'
+     )
  
  
  puts "#{Post.count}"
  Post.find_or_create_by(title: "Seeds Assignment Title", body: "Seeds Assignment Body")
  puts "#{Post.count}"
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
