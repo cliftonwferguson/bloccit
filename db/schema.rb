6d74e60fac937fa223b605198caf3f62704474d9
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-06-02 17:53:22 +0000

    topics spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190530232158) do
+ActiveRecord::Schema.define(version: 20190601234650) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20190530232158) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
