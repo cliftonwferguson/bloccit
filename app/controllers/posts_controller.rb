a224f9848ca5cfeedded4e6c51e3d5b48ae9b245
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-06-07 21:58:09 +0000

    Finished Authorization Assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,9 @@ class PostsController < ApplicationController
    before_action :require_sign_in, except: :show
    
    before_action :authorize_user, except: [:show, :new, :create]
+   
+   before_action :moderator_user, only: [:update]
+  
 
   def show
      @post = Post.find(params[:id])
@@ -72,6 +75,14 @@ class PostsController < ApplicationController
      unless current_user == post.user || current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to [post.topic, post]
+     end
+  end 
+  
+  def moderator_user 
+      post = Post.find(params[:id])
+      unless current_user == post.user || current_user.moderator? || current_user.admin?
+        flash[:alert] = "You must be a moderator to do that." 
+        redirect_to [post.topic, post]
      end
   end 
  
