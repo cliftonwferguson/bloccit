498210758b3108397a939d3293e80c2a95d43764
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-06-12 02:58:12 +0000

    post and comment count

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,13 @@
 module UsersHelper
     def does_user_have_post (current_user)
-       if current_user.posts.length > 0
-           return current_user.posts
-       else 
-            return "{current_user.name} has not submitted any posts yet."
-       end 
-    end 
-    
-    def does_user_have_comments (current_user)
-       if current_user.comments.length > 0
-           return current_user.comments
-       else
-           return "{current_user.name} has not submitted any comments yet."
+       if current_user.posts.length == 0
+           "#{current_user.name} has not submitted any posts yet."
        end
     end
     
+    def does_user_have_comments (current_user)
+       if current_user.comments.length == 0
+         return "#{current_user.name} has not submitted any comments yet."
+       end
+    end
 end
