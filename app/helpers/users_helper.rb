e6389fdac6989a8eab58347342012746dee4e617
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-06-12 01:05:32 +0000

    Users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,14 @@
 module UsersHelper
-    def does_user_have_post (user_id)
-       ## we will query the database to get a list of all posts that belong to the user
-         # if the list of post is empty,
-           #show "This user has no post" => return to the view.
-           #else iterate all user post and render a link to each one.
+    def does_user_have_post (user_id) do
+       if current_user == post.user
+            return post
+       else
+            return "This user does not have any posts."
+        end 
     end 
     
 end
+ ## we will query the database to get a list of all posts that belong to the user
+         # if the list of post is empty,
+           #show "This user has no post" => return to the view.
+           #else iterate all user post and render a link to each one.
